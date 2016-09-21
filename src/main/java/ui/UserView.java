48eca81ba0b1ffaadcd9933c102bec28c3d83a33
Keping <kepingwang@yahoo.com>
2016-09-21 13:49:47 -0400

    add basic user input control

--- src/main/java/ui/UserView.java
+++ src/main/java/ui/UserView.java
@@ -1,27 +1,54 @@
 package ui;
 
+import app.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class UserView extends Application {
 	
 	Stage stage; // the window
 	Scene scene; // the main content in the window
+	Controller ctrl;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		ctrl = new Controller();
 		stage = primaryStage;
-		stage.setTitle("First window for NLIDB");
+		stage.setTitle("Window for NLIDB");
 		
-		Label label1 = new Label("Welcome!");
+		Label label1 = new Label("Welcome to Natural Language Interface to DataBase!");
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(label1);
+		Label lblInput = new Label("Natural Language Input:");
+		TextField fieldIn = new TextField();
+		fieldIn.setPrefHeight(100);
 		
-		scene = new Scene(layout, 300, 200);
+		Button btnTranslate = new Button("translate");
+
+		Label lblSQL = new Label("SQL query:");
+		TextField fieldSQL = new TextField();
+		fieldSQL.setPrefHeight(100);
+		
+		// Define action of the translate button.
+		btnTranslate.setOnAction(e -> {
+			String queryMsg = ctrl.processNaturalLanguage(fieldIn.getText()).get();
+			fieldSQL.setText(queryMsg);
+		});
+		
+		VBox vb = new VBox();
+		vb.setSpacing(10);
+		vb.getChildren().addAll(
+				label1,
+				lblInput,fieldIn,
+				btnTranslate,
+				lblSQL, fieldSQL
+				);
+		
+		scene = new Scene(vb, 600, 450);
 		
 		stage.setScene(scene);
 		stage.show();
