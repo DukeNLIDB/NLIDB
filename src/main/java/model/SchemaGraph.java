eb9e37c3438e5204868014244b77e7c40e7c046b
YilinGao <yilin.gao@duke.edu>
2016-11-01 21:20:16 -0400

    revise column type in schema

--- src/main/java/model/SchemaGraph.java
+++ src/main/java/model/SchemaGraph.java
@@ -3,7 +3,6 @@ package model;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
