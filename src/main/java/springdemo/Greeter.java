76ef93ec27ee81d395fb98667ee8293c4ff6ae9a
Keping <kw238@duke.edu>
2017-10-04 16:43:56 -0400

    setup spring boot with gradle

---
+++ src/main/java/springdemo/Greeter.java
@@ -0,0 +1,7 @@
+package springdemo;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world";
+    }
+}
---
+++ src/main/java/springdemo/HelloWorld.java
@@ -0,0 +1,10 @@
+package springdemo;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+
+}
